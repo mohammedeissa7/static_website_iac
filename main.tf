44cbebd8c836e10803caf3342621415ab3932861
mohammed eissa7 <smme012345@gmail.com>
2025-01-23 12:09:28 +0000

    #1 generate_cli_scripts

--- main.tf
+++ main.tf
@@ -1,1 +1,43 @@
+terraform {
 
+  cloud {
+    organization = "eissa"
+    workspaces {
+      name = "terra-house"
+    }
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+provider "aws" {
+}
+provider "random" {
+  # Configuration options
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "bucket_name" {
+  lower = true
+  upper = false
+  length   = 32
+  special  = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  
+  bucket = random_string.bucket_name.result
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
