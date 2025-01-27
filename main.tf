d3bd4da0e265ffb99067bf4dce582fe68b82173d
mohammed eissa7 <smme012345@gmail.com>
2025-01-27 15:46:21 +0000

    #2 terraform is ready

--- main.tf
+++ main.tf
@@ -1,40 +1,46 @@
 terraform {
-
   cloud {
-    organization = "eissa"
+
+    organization = "example-org-5c51bc"
+
     workspaces {
       name = "terra-house"
     }
   }
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
     }
     aws = {
-      source = "hashicorp/aws"
-      version = "5.16.2"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
+
+
 provider "random" {
   # Configuration options
 }
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  lower = true
-  upper = false
-  length   = 32
-  special  = false
+  lower   = true
+  upper   = false
+  length  = 32
+  special = false
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
-  
+
   bucket = random_string.bucket_name.result
 }
 
