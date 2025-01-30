64e83c91d8b8c899b9a53369615da8c3c9bb65b8
mohammed eissa7 <smme012345@gmail.com>
2025-01-30 19:43:27 +0000

    #7 photos is in the website

--- main.tf
+++ main.tf
@@ -11,5 +11,6 @@ module "terrahouce_aws" {
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
   content_version = var.content_version
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "bucket_name" {
 output "website_endpoint" {
   value = module.terrahouce_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouce_aws.cloudfront_url
+}
